614020e0f04b1f622343f98eee79b352b69c6709
Your Name <221768963@edu.vut.ac.za>
2025-03-05 16:01:57 +0200

    modules

---
+++ models/Task.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    description: { type: String, required: true },
+    priority: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+    userId: { type: String, required: true } // Change from ObjectId to String
+});
+
+module.exports = mongoose.model("Task", taskSchema);
